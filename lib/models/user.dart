a9548d3eb7c794e3b6079bed8a43dd5c87a1cbe0
Kaio Martins <kaio.ccmartins@gmail.com>
2022-07-28 09:18:12 -0300

    Login system

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
   final String email;
   final String uid;
   final String photoUrl;
-  final String username;
+  final String? username;
   final String bio;
   final List followers;
   final List following;
@@ -16,6 +18,20 @@ class User {
     required this.followers,
     required this.following,
   });
+
+  static User fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+
+    return User(
+      username: snapshot["username"],
+      uid: snapshot["uid"],
+      email: snapshot["email"],
+      photoUrl: snapshot["photoUrl"],
+      bio: snapshot["bio"],
+      followers: snapshot["followers"],
+      following: snapshot["following"],
+    );
+  }
 
   //converte a classe para Object Files - Json
   Map<String, dynamic> toJson() => {
