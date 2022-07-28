20dc82f6b1111637ceed9e6e0ce08047f2c1db12
Kaio Martins <kaio.ccmartins@gmail.com>
2022-07-28 14:15:20 -0300

    Build error solved, but now it does not return the user on screen

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,7 +4,7 @@ class User {
   final String email;
   final String uid;
   final String photoUrl;
-  final String? username;
+  final String username;
   final String bio;
   final List followers;
   final List following;
--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/providers/user_provider.dart';
 import 'package:provider/provider.dart';
@@ -17,11 +17,11 @@ class MobileScreenLayout extends StatefulWidget {
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   @override
   Widget build(BuildContext context) {
-    final User? user = Provider.of<UserProvider>(context).getUser as User?;
+    model.User? user = Provider.of<UserProvider>(context).getUser;
 
     return Scaffold(
       body: Center(
-        child: Text(user.username),
+        child: Text("${user?.username}"),
       ),
     );
   }
